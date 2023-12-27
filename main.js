0e939020ebeb025ae0fe0da9a389ec0429581dbd
Alexander Lane <alexandertlane@gmail.com>
2023-12-27 18:05:30 -0500

    Working demo, transitioning to actual layout

--- main.js
+++ main.js
@@ -33,6 +33,7 @@ window.addEventListener('load', function() {
             // groups[z].children[0].setAttribute('fill', 'purple');
             setRandomColor(groups[z].children[0]);
         }
+        groups[3].style.visibility = "hidden";
         
         // var bob = svgDoc.getElementById('bob');
         // bob.children[0].setAttribute('fill', 'green');
@@ -40,14 +41,83 @@ window.addEventListener('load', function() {
 
     function applyPreset(targetID) {
         var presetToApply = presets[targetID];
-        groups[0].children[0].setAttribute('fill', presetToApply.tl);
-        groups[1].children[0].setAttribute('fill', presetToApply.tr);
-        groups[2].children[0].setAttribute('fill', presetToApply.bl);
-        groups[3].children[0].setAttribute('fill', presetToApply.br);
+        groups[0].children[0].fill=  presetToApply.tl;
+        groups[1].children[0].fill=  presetToApply.tr;
+        groups[2].children[0].fill= presetToApply.bl;
+        groups[3].children[0].fill= presetToApply.br;
+        // groups[3].children[0].setAttribute('fill', presetToApply.br);
         
     }
 });//end on load
+/*
 
+array per row
+array each for mutes and auditions
+array of all other buttons
+all of the above in a larger array or object
+
+OR
+
+object with each button outside the grid, 
+then either coordinates per grid button or arrays per row
+
+/////
+
+Assuming object: deluge with properties like deluge.playButton
+Recolor function: 
+Func(button, preset){
+ deluge[button].fill = preset[button];
+}
+
+Or
+
+For (let button in deluge) {
+ deluge[button].fill = preset[button]
+}
+
+Actually the second one could be recolor all and use the first one. 
+Apply preset per button or to all. Recolor function same except second parameter is a color
+
+
+
+## TO DO: 
+Confirm all elements are nested the same way so it's always namedID.children[0] that has the fill color? Easier to reconfigure svg if need be than to have a ton of validating in function probably 
+
+SVG size outlined text vs not
+
+Couple of starting preset buttons 
+
+OnClick for svg buttons (determine which, capture vs bubble etc)
+
+In SVG, will need classes as well as IDs so all clip rows can share a behavior, for instance 
+
+Need a way to keep track of state so when eventually buttons are interactive they correctly apply the correct preset based on current view and what was pressed, eg, pressing song from clip goes to song view, but from song view goes to arranger
+
+How to turn knobs? Click and drag? To push turn click then click drag?
+
+
+### PRESETS: 
+
+#### Clip
+Default
+Shift/aud pushed
+<> pushed
+^v pushed
+
+#### Song  
+Same as clip, plus
+Clip pad held
+
+#### Arranger
+Most of what clip has
+
+#### Grid
+TBD
+
+#### Automation
+TBD
+
+*/
   
 function TestPreset(tl, tr, bl, br) {
     this.tl = tl;
